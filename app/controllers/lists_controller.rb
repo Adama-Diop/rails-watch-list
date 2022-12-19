0f0fca05ea74b15864f48c878aafe7676bc52822
Adama Diop <adamadiop2015@outlook.com>
2022-12-19 14:32:13 +0100

    la watchlist est presque terminé il manque à faire les reviews et le front

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -19,12 +19,13 @@ class ListsController < ApplicationController
 
   def show
     @list = List.find(params[:id])
+    @bookmark = Bookmark.new
   end
 
   def destroy
     @list = List.find(params[:id])
     @list.destroy
-    redirect_to robots_path, status: :see_other
+    redirect_to lists_path, status: :see_other
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "lists#index"
-  resources :lists, only: [:new, :create, :show, :destroy]
+  root to: "lists#index"
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  resources :bookmarks, only: :destroy
 end
